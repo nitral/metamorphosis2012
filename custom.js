41a419da22f79f3dc26f62feeb301b59360b1615
Nilay Binjola <nilaybinjola@gmail.com>
2012-10-21 18:35:09 +0530

    Simulator: First functional version of the webpage.  - Layout is Completed. Dummy Text added.  - Formatting CSS File added.  - Javascript for GlowBar Effects added.  - Modified Javascript for TabBar Effects.  - Modified CSS for smoother Layout.  - Cleaned up images/

---
+++ custom.js
@@ -0,0 +1,98 @@
+/* ********************
+JS FOR METAMORPHOSIS
+******************** */
+
+var HomeContent = "<h2>HOME</h2><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vulputate, diam eu cursus aliquam, eros nisl commodo libero, non interdum nisi urna eu felis. Aliquam nisi turpis, gravida non egestas nec, eleifend hendrerit tellus. In vitae justo quis massa iaculis hendrerit sodales sit amet velit. Morbi blandit odio non ante dapibus ut dictum arcu dictum. Nunc et est at lectus pretium egestas eget adipiscing urna. Pellentesque auctor, dolor ut congue feugiat, ipsum eros pharetra sapien, eu aliquet velit neque vitae mauris. Donec sit amet orci id ante porttitor adipiscing quis quis mauris. Vestibulum tellus urna, lacinia quis consectetur nec, blandit vitae quam. Cras sit amet est sapien. Aliquam erat volutpat. Aliquam nec tellus ut justo tincidunt imperdiet. Vestibulum tristique, orci quis pulvinar luctus, lectus mi consequat nunc, ac volutpat leo dui eget orci. In hac habitasse platea dictumst. Pellentesque et sapien sit amet nisi gravida faucibus in gravida felis.</p><p>Proin in mattis magna. Duis turpis risus, malesuada id faucibus eu, venenatis at risus. Morbi cursus orci venenatis dui malesuada interdum. Vestibulum nec erat elit, at adipiscing nisi. Aliquam aliquam condimentum justo, id pretium enim varius id. Aliquam velit magna, malesuada eget hendrerit ac, iaculis sit amet ligula. Donec sit amet massa quis nulla varius laoreet vitae pellentesque justo.</p><p>Duis vitae sem ultrices metus vestibulum fermentum id eget orci. Pellentesque placerat convallis nulla, sit amet sagittis sem vehicula eu. Morbi in diam a lacus sollicitudin eleifend eget bibendum sapien. Vivamus feugiat tincidunt nibh, et tincidunt sem placerat fringilla. Duis sed purus sit amet nisl convallis luctus eget in tortor. Duis lectus eros, egestas vitae venenatis eget, dictum egestas orci. Nulla facilisi. Duis quis est eros, eget sagittis sem. Nulla consectetur iaculis facilisis. Cras feugiat aliquet ultricies. Mauris sed dolor mi, sit amet luctus turpis. Donec vestibulum quam ac lectus ultricies sit amet imperdiet arcu viverra. Pellentesque volutpat semper velit quis blandit.</p><p>Vestibulum vel eros id erat hendrerit faucibus vel eget purus. Ut scelerisque viverra arcu, quis faucibus erat venenatis vel. Mauris consectetur libero quis orci gravida posuere. Integer adipiscing lectus ac magna interdum in sagittis dolor rhoncus. Nulla sollicitudin tempus mauris, non lacinia ligula dignissim et. Phasellus aliquet purus sodales mauris gravida luctus. Etiam nec risus ultrices est tincidunt vehicula vel et sapien.</p>";
+var AboutContent = "<h2>ABOUT</h2><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vulputate, diam eu cursus aliquam, eros nisl commodo libero, non interdum nisi urna eu felis. Aliquam nisi turpis, gravida non egestas nec, eleifend hendrerit tellus. In vitae justo quis massa iaculis hendrerit sodales sit amet velit. Morbi blandit odio non ante dapibus ut dictum arcu dictum. Nunc et est at lectus pretium egestas eget adipiscing urna. Pellentesque auctor, dolor ut congue feugiat, ipsum eros pharetra sapien, eu aliquet velit neque vitae mauris. Donec sit amet orci id ante porttitor adipiscing quis quis mauris. Vestibulum tellus urna, lacinia quis consectetur nec, blandit vitae quam. Cras sit amet est sapien. Aliquam erat volutpat. Aliquam nec tellus ut justo tincidunt imperdiet. Vestibulum tristique, orci quis pulvinar luctus, lectus mi consequat nunc, ac volutpat leo dui eget orci. In hac habitasse platea dictumst. Pellentesque et sapien sit amet nisi gravida faucibus in gravida felis.</p><p>Proin in mattis magna. Duis turpis risus, malesuada id faucibus eu, venenatis at risus. Morbi cursus orci venenatis dui malesuada interdum. Vestibulum nec erat elit, at adipiscing nisi. Aliquam aliquam condimentum justo, id pretium enim varius id. Aliquam velit magna, malesuada eget hendrerit ac, iaculis sit amet ligula. Donec sit amet massa quis nulla varius laoreet vitae pellentesque justo.</p><p>Duis vitae sem ultrices metus vestibulum fermentum id eget orci. Pellentesque placerat convallis nulla, sit amet sagittis sem vehicula eu. Morbi in diam a lacus sollicitudin eleifend eget bibendum sapien. Vivamus feugiat tincidunt nibh, et tincidunt sem placerat fringilla. Duis sed purus sit amet nisl convallis luctus eget in tortor. Duis lectus eros, egestas vitae venenatis eget, dictum egestas orci. Nulla facilisi. Duis quis est eros, eget sagittis sem. Nulla consectetur iaculis facilisis. Cras feugiat aliquet ultricies. Mauris sed dolor mi, sit amet luctus turpis. Donec vestibulum quam ac lectus ultricies sit amet imperdiet arcu viverra. Pellentesque volutpat semper velit quis blandit.</p><p>Vestibulum vel eros id erat hendrerit faucibus vel eget purus. Ut scelerisque viverra arcu, quis faucibus erat venenatis vel. Mauris consectetur libero quis orci gravida posuere. Integer adipiscing lectus ac magna interdum in sagittis dolor rhoncus. Nulla sollicitudin tempus mauris, non lacinia ligula dignissim et. Phasellus aliquet purus sodales mauris gravida luctus. Etiam nec risus ultrices est tincidunt vehicula vel et sapien.</p>";
+var RulesContent = "<h2>RULES</h2><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vulputate, diam eu cursus aliquam, eros nisl commodo libero, non interdum nisi urna eu felis. Aliquam nisi turpis, gravida non egestas nec, eleifend hendrerit tellus. In vitae justo quis massa iaculis hendrerit sodales sit amet velit. Morbi blandit odio non ante dapibus ut dictum arcu dictum. Nunc et est at lectus pretium egestas eget adipiscing urna. Pellentesque auctor, dolor ut congue feugiat, ipsum eros pharetra sapien, eu aliquet velit neque vitae mauris. Donec sit amet orci id ante porttitor adipiscing quis quis mauris. Vestibulum tellus urna, lacinia quis consectetur nec, blandit vitae quam. Cras sit amet est sapien. Aliquam erat volutpat. Aliquam nec tellus ut justo tincidunt imperdiet. Vestibulum tristique, orci quis pulvinar luctus, lectus mi consequat nunc, ac volutpat leo dui eget orci. In hac habitasse platea dictumst. Pellentesque et sapien sit amet nisi gravida faucibus in gravida felis.</p><p>Proin in mattis magna. Duis turpis risus, malesuada id faucibus eu, venenatis at risus. Morbi cursus orci venenatis dui malesuada interdum. Vestibulum nec erat elit, at adipiscing nisi. Aliquam aliquam condimentum justo, id pretium enim varius id. Aliquam velit magna, malesuada eget hendrerit ac, iaculis sit amet ligula. Donec sit amet massa quis nulla varius laoreet vitae pellentesque justo.</p><p>Duis vitae sem ultrices metus vestibulum fermentum id eget orci. Pellentesque placerat convallis nulla, sit amet sagittis sem vehicula eu. Morbi in diam a lacus sollicitudin eleifend eget bibendum sapien. Vivamus feugiat tincidunt nibh, et tincidunt sem placerat fringilla. Duis sed purus sit amet nisl convallis luctus eget in tortor. Duis lectus eros, egestas vitae venenatis eget, dictum egestas orci. Nulla facilisi. Duis quis est eros, eget sagittis sem. Nulla consectetur iaculis facilisis. Cras feugiat aliquet ultricies. Mauris sed dolor mi, sit amet luctus turpis. Donec vestibulum quam ac lectus ultricies sit amet imperdiet arcu viverra. Pellentesque volutpat semper velit quis blandit.</p><p>Vestibulum vel eros id erat hendrerit faucibus vel eget purus. Ut scelerisque viverra arcu, quis faucibus erat venenatis vel. Mauris consectetur libero quis orci gravida posuere. Integer adipiscing lectus ac magna interdum in sagittis dolor rhoncus. Nulla sollicitudin tempus mauris, non lacinia ligula dignissim et. Phasellus aliquet purus sodales mauris gravida luctus. Etiam nec risus ultrices est tincidunt vehicula vel et sapien.</p>";
+var ParticipateContent = "<h2>PARTICIPATE</h2><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vulputate, diam eu cursus aliquam, eros nisl commodo libero, non interdum nisi urna eu felis. Aliquam nisi turpis, gravida non egestas nec, eleifend hendrerit tellus. In vitae justo quis massa iaculis hendrerit sodales sit amet velit. Morbi blandit odio non ante dapibus ut dictum arcu dictum. Nunc et est at lectus pretium egestas eget adipiscing urna. Pellentesque auctor, dolor ut congue feugiat, ipsum eros pharetra sapien, eu aliquet velit neque vitae mauris. Donec sit amet orci id ante porttitor adipiscing quis quis mauris. Vestibulum tellus urna, lacinia quis consectetur nec, blandit vitae quam. Cras sit amet est sapien. Aliquam erat volutpat. Aliquam nec tellus ut justo tincidunt imperdiet. Vestibulum tristique, orci quis pulvinar luctus, lectus mi consequat nunc, ac volutpat leo dui eget orci. In hac habitasse platea dictumst. Pellentesque et sapien sit amet nisi gravida faucibus in gravida felis.</p><p>Proin in mattis magna. Duis turpis risus, malesuada id faucibus eu, venenatis at risus. Morbi cursus orci venenatis dui malesuada interdum. Vestibulum nec erat elit, at adipiscing nisi. Aliquam aliquam condimentum justo, id pretium enim varius id. Aliquam velit magna, malesuada eget hendrerit ac, iaculis sit amet ligula. Donec sit amet massa quis nulla varius laoreet vitae pellentesque justo.</p><p>Duis vitae sem ultrices metus vestibulum fermentum id eget orci. Pellentesque placerat convallis nulla, sit amet sagittis sem vehicula eu. Morbi in diam a lacus sollicitudin eleifend eget bibendum sapien. Vivamus feugiat tincidunt nibh, et tincidunt sem placerat fringilla. Duis sed purus sit amet nisl convallis luctus eget in tortor. Duis lectus eros, egestas vitae venenatis eget, dictum egestas orci. Nulla facilisi. Duis quis est eros, eget sagittis sem. Nulla consectetur iaculis facilisis. Cras feugiat aliquet ultricies. Mauris sed dolor mi, sit amet luctus turpis. Donec vestibulum quam ac lectus ultricies sit amet imperdiet arcu viverra. Pellentesque volutpat semper velit quis blandit.</p><p>Vestibulum vel eros id erat hendrerit faucibus vel eget purus. Ut scelerisque viverra arcu, quis faucibus erat venenatis vel. Mauris consectetur libero quis orci gravida posuere. Integer adipiscing lectus ac magna interdum in sagittis dolor rhoncus. Nulla sollicitudin tempus mauris, non lacinia ligula dignissim et. Phasellus aliquet purus sodales mauris gravida luctus. Etiam nec risus ultrices est tincidunt vehicula vel et sapien.</p>";
+var CurrentActiveContent = "";
+
+function showContent(contentID)
+{
+	showNavTab(contentID);
+	
+	if(contentID == "Home")
+	{
+		document.getElementById("contentHolder").innerHTML = HomeContent;
+		CurrentActiveContent = "Home";
+	}
+	else if(contentID == "About")
+	{
+		document.getElementById("contentHolder").innerHTML = AboutContent;
+		CurrentActiveContent = "About";
+	}
+	else if(contentID == "Rules")
+	{
+		document.getElementById("contentHolder").innerHTML = RulesContent;
+		CurrentActiveContent = "Rules";
+	}
+	else if(contentID == "Participate")
+	{
+		document.getElementById("contentHolder").innerHTML = ParticipateContent;
+		CurrentActiveContent = "Participate";
+	}
+	
+	modifyFontFace(contentID);
+	navBarLinksHoverEffect(contentID);
+}
+
+function showNavTab(contentID)
+{
+	document.getElementById("navTabHome").style.visibility = "hidden";
+	document.getElementById("navTabAbout").style.visibility = "hidden";
+	document.getElementById("navTabRules").style.visibility = "hidden";
+	document.getElementById("navTabParticipate").style.visibility = "hidden";
+	
+	var navTabID = "navTab" + contentID;
+	
+	document.getElementById(navTabID).style.visibility = "visible";
+}
+
+
+function modifyFontFace(contentID)
+{
+	var navBarLinkID = "nav" + contentID;
+	
+	document.getElementById("navHome").style.color = "#000000";
+	document.getElementById("navAbout").style.color = "#000000";
+	document.getElementById("navRules").style.color = "#000000";
+	document.getElementById("navParticipate").style.color = "#000000";
+	document.getElementById("navHome").style.textShadow = "0px 0px 0px #000000";
+	document.getElementById("navAbout").style.textShadow = "0px 0px 0px #000000";
+	document.getElementById("navRules").style.textShadow = "0px 0px 0px #000000";
+	document.getElementById("navParticipate").style.textShadow = "0px 0px 0px #000000";
+	
+	document.getElementById(navBarLinkID).style.color = "#3fc7dd";
+	document.getElementById(navBarLinkID).style.textShadow = "0px 0px 6px #3fc7dd";
+}
+
+function navBarLinksHoverEffect(contentID)
+{
+	document.getElementById("glowBarHome").src = "./images/glowBarInactive.png";
+	document.getElementById("glowBarAbout").src = "./images/glowBarInactive.png";
+	document.getElementById("glowBarRules").src = "./images/glowBarInactive.png";
+	document.getElementById("glowBarParticipate").src = "./images/glowBarInactive.png";
+	document.getElementById("glowBarHome").style.boxShadow = "0px 0px 0px #000000";
+	document.getElementById("glowBarAbout").style.boxShadow = "0px 0px 0px #000000";
+	document.getElementById("glowBarRules").style.boxShadow = "0px 0px 0px #000000";
+	document.getElementById("glowBarParticipate").style.boxShadow = "0px 0px 0px #000000";
+	
+	if(contentID == 0)
+	{
+		var CurrentActiveGlowBar = "glowBar" + CurrentActiveContent;
+		var CurrentActiveNavContent = "nav" + CurrentActiveContent;
+		document.getElementById(CurrentActiveGlowBar).src = "./images/glowBarActive.png";
+		document.getElementById(CurrentActiveGlowBar).style.boxShadow = "0px 0px 12px #3fc7dd";
+		document.getElementById(CurrentActiveNavContent).style.color = "#3fc7dd";
+		document.getElementById(CurrentActiveNavContent).style.textShadow = "0px 0px 6px #3fc7dd";
+		modifyFontFace(CurrentActiveContent);
+	}
+	else
+	{
+		var glowBarID = "glowBar" + contentID;
+		document.getElementById(glowBarID).src = "./images/glowBarActive.png";
+		document.getElementById(glowBarID).style.boxShadow = "0px 0px 12px #3fc7dd";
+		modifyFontFace(contentID);
+	}
+}
